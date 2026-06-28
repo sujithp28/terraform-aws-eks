3233b4402048a6c44b773a115d2cef1b752efe83
Sujith <70098023+sujithp28@users.noreply.github.com>
2026-06-28 15:55:24 +0530

    feat: Add EKS OIDC/IRSA provider

---
+++ modules/eks/oidc.tf
@@ -0,0 +1,15 @@
+# OIDC Provider for EKS (required for IRSA - IAM Roles for Service Accounts)
+
+data "aws_caller_identity" "current" {}
+
+data "tls_certificate" "cluster" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cluster.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+  tags = merge(var.tags, { Name = "${var.cluster_name}-oidc" })
+}
